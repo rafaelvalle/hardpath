71a91eb0974e1c7c4129a770141b1ce57db1f8e7
Rafael Valle <jrafaelvalle@gmail.com>
2017-04-06 20:06:55 -0700

    increasing variance of augmented data

--- cnn_ocr.py
+++ cnn_ocr.py
@@ -67,12 +67,12 @@ batch_size = 128
 train_datagen = ImageDataGenerator(
     featurewise_center=True,
     featurewise_std_normalization=True,
-    rotation_range=20,
-    shear_range=0.2,
-    zoom_range=0.2,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    horizontal_flip=True,
+    rotation_range=30,
+    shear_range=0.3,
+    zoom_range=0.4,
+    width_shift_range=0.4,
+    height_shift_range=0.4,
+    horizontal_flip=False,
     data_format='channels_first')
 # estimate std, mean, ZCA from baseline data
 train_datagen.fit(letters)
